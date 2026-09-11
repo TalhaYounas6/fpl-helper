99cc0bbe88f7dd5c8c157e15f564a3e9b225646c
TalhaYounas6 <myounas1168@gmail.com>
2026-09-11 21:13:50 +0500

    changed extractor_args

--- server/services/audioService.js
+++ server/services/audioService.js
@@ -27,7 +27,7 @@ export const downloadAudio = async(videoId)=>{
       noPlaylist: true,
       ffmpegLocation: ffmpegPath,
       jsRuntimes: 'node', 
-      extractorArgs: 'youtube:player_client=android',
+      extractorArgs: 'youtube:player_client=tv',
     })
         
     console.log("Audio download complete.For: ",outputPath);
