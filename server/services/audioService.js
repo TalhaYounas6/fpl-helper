77794b3b5ad8f5451509a62a4c1259876d4e1ad3
TalhaYounas6 <myounas1168@gmail.com>
2026-09-23 00:26:37 +0500

    audio service

--- server/services/audioService.js
+++ server/services/audioService.js
@@ -30,7 +30,7 @@ export const downloadAudio = async(videoId)=>{
       output: path.join(temp_dir, '%(id)s.%(ext)s'), 
       noPlaylist: true,
       ffmpegLocation: ffmpegPath, 
-      cookies: path.join(__dirname,'./cookies.txt')
+      cookies: path.join(__dirname,'../cookies.txt')
     })
         
     console.log("Audio download complete.For: ",outputPath);
